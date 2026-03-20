89d91b355520d4385ca3e240dda793cf20c2211f
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-03-20 23:32:59 +0530

    final

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-<<<<<<< HEAD
 // Use the logger and error handling middleware for all routes
 app.use(logger);
 // global error handler
 app.use(errorHandler);
 
 // Import and use the task routes
-=======
->>>>>>> 926bddd2a4d86c937c0e70b13dd5f59025fc515f
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,7 +16,5 @@ app.listen(port, (err) => {
     }
     console.log(`Server is listening on ${port}`);
 });
-
-
 
 module.exports = app;
